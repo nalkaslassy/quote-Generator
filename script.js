eb36ed814b04c0c566ecf7a64daadbca7e27ba17
Nadav Alkaslassy <nalkaslassy@gmail.com>
2021-08-01 22:05:42 -0400

    Quote-Generator

--- script.js
+++ script.js
@@ -13,13 +13,19 @@ function newQuote() {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];  
 // Check if Author field is blank and replace it with 'Unknown'
 if (!quote.author){
-    authorText.textContent = 'Unkown'
+    authorText.textContent = 'Unkown';
 }
 else {
     authorText.textContent = quote.author;
 }
+// Check quote length to determine the type of style
+if (quote.text.length > 120){
+    quoteText.classList.add('long-quote');
 
-
+    }
+    else {
+        quoteText.classList.remove('long-quote');
+    }
 
 quoteText.textContent = quote.text;
 }
@@ -35,6 +41,13 @@ newQuote();
         //Error Handling                                                                                    
     }
 }
+// Tweet Quote
+function tweetQuote() {
+const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+window.open(twitterUrl, '_blank');
+}
+// Even lisiteners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 //  On Load
-
 getQuotes();
